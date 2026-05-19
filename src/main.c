ce94b7387df891b95d78726f7665ccdda5d88133
PokuG@Yarufu <yarufu@pokug.net>
2026-05-19 22:05:42 +0900

    BGM読み込み失敗時のログ出力を追加

--- src/main.c
+++ src/main.c
@@ -642,7 +642,7 @@ static void ui_draw_stand(enum StandId stand_id, enum FaceId face_id,
 
     if (!graph98_draw_sprite_file_trans(sprite_path, x, y, 0)) {
         debug_log("sprite load failed: %s", sprite_path);
-        
+
         // ui_draw_stand_placeholder(x, y);
         graph98_boxfill(x + 20, y + 20, x + 180, y + 80, 4);
         graph98_draw_string(x + 30, y + 45, "SPRITE LOAD NG", 15);
@@ -1945,6 +1945,8 @@ static void run_script_sjis(void)
                             strcpy(g_state.bgm, arg1);
 
                             pmd_start_music();
+                        }else{
+                            debug_log("bgm load failed: %s", arg1);
                         }
                     }
                 }

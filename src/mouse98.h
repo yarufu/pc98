fed4cfb95871c94eacdbf432b021e8dc2d5c8ac8
PokuG@Yarufu <yarufu@pokug.net>
2026-06-11 21:18:37 +0900

    タイトルBGMとマウスメニュー操作を実装

--- src/mouse98.h
+++ src/mouse98.h
@@ -4,5 +4,7 @@
 int mouse98_init(void);
 int mouse98_left_pressed(void);
 void mouse98_wait_left_release(void);
+void mouse98_get_motion(int *dx, int *dy);
+void mouse98_hide_cursor(void);
 
 #endif
